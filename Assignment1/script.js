16dab5f9fbc2349d40c9201f02aaa5bc23b43240
AbuZar <abuzarwork33@gmail.com>
2026-04-15 06:36:19 -0700

    Complete assignment1

---
+++ Assignment1/script.js
@@ -0,0 +1,36 @@
+// Q1
+alert("Welcome to our website!");
+document.write("<p>Welcome to our website!</p>");
+
+// Q2
+alert("Error! Please enter a valid password.");
+document.write("<p>Error! Please enter a valid password.</p>");
+
+// Q3
+alert("Welcome to JS Land...\nHappy Coding!");
+document.write("<p>Welcome to JS Land...<br>Happy Coding!</p>");
+
+// Q4
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+document.write("<p>Welcome to JS Land...<br>Happy Coding!</p>");
+
+// Q5
+console.log("Hello... I can run JS through my web browser's console");
+document.write("<p>Message also printed in console</p>");
+
+// Q6
+alert("Welcome to my website!");
+document.write("<p>Welcome to my website! (Q6)</p>");
+
+// Q7 (Head wala ab yahan sirf text show karega)
+document.write("<h3>Script in External JS</h3>");
+
+// Q7 (Inside Body)
+document.write("<p>Inside Body Script</p>");
+
+// Q7 (Before Content)
+document.write("<p>Before Page Content</p>");
+
+// Q7 (End of Body)
+document.write("<p>After Page Content</p>");
